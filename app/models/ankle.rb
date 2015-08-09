0e6f35292f83f99cc13f4dd03ab359e04d799917
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-08-10 07:56:21 +0900

    fix conflict

--- app/models/ankle.rb
+++ app/models/ankle.rb
@@ -2,6 +2,7 @@ class Ankle < ActiveRecord::Base
   has_many :win_battles, :class_name => "Battle", :foreign_key => "win_ankle_id"
   has_many :lose_battles, :class_name => "Battle", :foreign_key => "lose_ankle_id"
   has_many :comments
+  has_many :favorites
   belongs_to :user
   mount_uploader :image, ImageUploader
 end
