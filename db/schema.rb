ec57c225ff3b31809df6660cfd7c334a8bae86f6
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-08-10 05:03:47 +0900

    finish compre system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150809155257) do
 
   create_table "ankles", force: :cascade do |t|
