ae812afb4dde55c8e4f712bcfb068df758883baa
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-08-10 02:55:52 +0900

    fix comment

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -5,7 +5,16 @@ class Battle < ActiveRecord::Base
 
   def save_with_cal
     dr = 16 + (loser.score - winner.score) * 0.04
+    if dr < 1
+      dr = 1
+    elsif dr > 31
+      dr = 31
+    end
     winner.score = winner.score + dr
+    loser.score = loser.score - dr
 
+    winner.save
+    loser.save
+    save
   end
 end
