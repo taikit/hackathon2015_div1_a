18aa4a0958f965faef111212492e68402ac8ebbb
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-08-10 10:57:49 +0900

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
                        :sessions => 'users/sessions',
                        :registrations => 'users/registrations'
                    }
-
+  resources :users, :only => [:show]
   resources :ankles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
